0dff1e61e0989fe0ea58e7d157d888a6aa7a7b32
John Hampton <john_j_hampton@yahoo.com>
2015-06-16 15:57:34 -0400

    Add toggleComplete event handler in TaskView definition, toggles CSS class and isComplete property on model

--- scripts/views/taskView.js
+++ scripts/views/taskView.js
@@ -2,12 +2,22 @@ export default Backbone.View.extend ({
   template: JST.task,
   tagName: 'li',
 
+  events: {
+    'click .toggle': 'toggleCompleted'
+  },
+
   initialize: function() {
     this.render();
   },
 
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
+  },
+
+  toggleCompleted: function() {
+    var completedStatus = this.model.get('isComplete');
+    this.$el.toggleClass('completed', !completedStatus);
+    this.model.set('isComplete', !completedStatus);
   }
 
 });
